d842b0c756a2a52be00554bf8165a8f135645b0a
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-07-07 16:08:24 +0300

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,83 @@
+// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне. Посмотри демо видео работы галереи.
+
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// Разметка элемента галереи​
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+{
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+}
+// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
+
+// Закрытие с клавиатуры​
+// ВНИМАНИЕ
+// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+// Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+container.insertAdjacentHTML('beforeend', createImageCards(galleryItems));
+
+function createImageCards(arry) {
+  return arry
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
+  </a>
+  </div>
+    `;
+    })
+    .join('');
+}
+const link = document.querySelectorAll('.gallery__link');
+
+function linkDisable(elements) {
+  for (let element of elements) {
+    element.addEventListener('click', function (event) {
+      event.preventDefault();
+    });
+  }
+}
+linkDisable(link);
+
+container.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" >
+`);
+
+  instance.show();
+}
